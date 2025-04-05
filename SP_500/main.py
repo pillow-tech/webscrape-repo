d04507e196fd3413dd99a26e8291d8bd9ffbc6c8
pillow-tech <tj.contreras24@gmail.com>
2025-04-05 22:46:19 +0800

    Add data scraping for stock informations displayed in Yahoo Finance Page

--- SP_500/main.py
+++ SP_500/main.py
@@ -18,6 +18,25 @@ def get_SP500():
         symbols_arr.append(symbol)
     return symbols_arr
 
+def extract_details(stock_symbols):
+    
+    stock_arr = []
+    for symbol in stock_symbols:
+        stock_values = []
+        url = "https://finance.yahoo.com/quote/" + symbol
+        headers = {'User-agent': 'Not a bot'}
+
+        response = requests.get(url, headers= headers)
+        soup = BeautifulSoup(response.text, features="html.parser")
+        li_extract = soup.find_all("li", class_="yf-1jj98ts")
+
+        for tag in li_extract:
+            value = tag.contents[2].text
+            stock_values.append(value)
+        
+        stock_arr.append(stock_values)
+    return stock_arr
+
 
 if __name__ == "__main__":
     sp_500 = get_SP500()
